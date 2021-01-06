04dee7c8ec0a498907d284d0525ad52f23376fb1
Mike Spahr <47612192+spahrm@users.noreply.github.com>
2021-01-06 12:12:18 +0100

    Added first implementation of the HaeufleTendonForceLengthCurve as described in Haeufle 2014 as Fsee

---
+++ OpenSim/Actuators/HaeufleTendonForceLengthCurve.cpp
@@ -0,0 +1,148 @@
+/* -------------------------------------------------------------------------- *
+ *                    OpenSim:  HaeufleTendonForceLengthCurve.cpp             *
+ * -------------------------------------------------------------------------- *
+ * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
+ * See http://opensim.stanford.edu and the NOTICE file for more information.  *
+ * OpenSim is developed at Stanford University and supported by the US        *
+ * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
+ * through the Warrior Web program.                                           *
+ *                                                                            *
+ * Copyright (c) 2005-2017 Stanford University and the Authors                *
+ * Author(s): Mike Spahr                                                      *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.         *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
+#include "HaeufleTendonForceLengthCurve.h"
+
+using namespace OpenSim;
+using namespace SimTK;
+using namespace std;
+
+HaeufleTendonForceLengthCurve::HaeufleTendonForceLengthCurve() 
+{
+    setNull();
+    constructProperties();
+    setName(getConcreteClassName());
+}
+
+HaeufleTendonForceLengthCurve::HaeufleTendonForceLengthCurve(
+    double serialElasticRestLength,
+    double forceAtNonlinearLinearTransition) 
+{
+    setNull();
+    constructProperties();
+    setName(getConcreteClassName());
+
+    set_serial_elastic_rest_length(serialElasticRestLength);
+    set_force_at_nonlinear_linear_transition(forceAtNonlinearLinearTransition);
+
+}
+
+HaeufleTendonForceLengthCurve::HaeufleTendonForceLengthCurve(
+    double serialElasticRestLength,
+    double relativeStretchAtNonlinearLinearTransition,
+    double relativeStretchAtLinearPart,
+    double forceAtNonlinearLinearTransition) 
+{
+    setNull();
+    constructProperties();
+    setName(getConcreteClassName());
+
+    set_serial_elastic_rest_length(serialElasticRestLength);
+    set_relative_stretch_at_linear_part(relativeStretchAtLinearPart);
+    set_relative_stretch_at_nonlinear_linear_transition(
+            relativeStretchAtNonlinearLinearTransition);
+    set_force_at_nonlinear_linear_transition(forceAtNonlinearLinearTransition);
+}
+
+void HaeufleTendonForceLengthCurve::setNull() 
+{ setAuthors("Mike Spahr"); }
+
+void HaeufleTendonForceLengthCurve::constructProperties() 
+{
+    constructProperty_serial_elastic_rest_length(0.15);
+    constructProperty_relative_stretch_at_nonlinear_linear_transition(0.0425);
+    constructProperty_relative_stretch_at_linear_part(0.0170);
+    constructProperty_force_at_nonlinear_linear_transition(500);
+}
+
+
+
+double HaeufleTendonForceLengthCurve::getSerialElasticRestLength() const {
+    return get_serial_elastic_rest_length();
+}
+
+double
+HaeufleTendonForceLengthCurve::getRelativeStretchAtNonlinearLinearTransition()
+const {
+    return get_relative_stretch_at_nonlinear_linear_transition();
+
+}
+
+
+double HaeufleTendonForceLengthCurve::getRelativeStretchAtLinearPart() const {
+    return get_relative_stretch_at_linear_part();
+}
+
+double
+HaeufleTendonForceLengthCurve::getForceAtNonlinearLinearTransition() const {
+    return get_force_at_nonlinear_linear_transition();
+}
+
+void HaeufleTendonForceLengthCurve::setSerialElasticRestLength(
+    double aSerialElasticRestLength) const {
+    set_serial_elastic_rest_length(aSerialElasticRestLength);
+}
+
+
+void HaeufleTendonForceLengthCurve::setForceAtNonlinearLinearTransition(
+    double aForceAtNonlinearLinearTransition) const {
+    set_force_at_nonlinear_linear_transition(aForceAtNonlinearLinearTransition);
+}
+
+void HaeufleTendonForceLengthCurve::setRelativeStretchAtNonlinearLinearTransition(
+    double aRelativeStretchAtNonlinearLinearTransition) const {
+    set_relative_stretch_at_nonlinear_linear_transition(
+            aRelativeStretchAtNonlinearLinearTransition);
+}
+
+
+void HaeufleTendonForceLengthCurve::setRelativeStretchAtLinearPart(
+    double aRelativeStretchAtLinearPart) const {
+    set_relative_stretch_at_linear_part(aRelativeStretchAtLinearPart);
+}
+
+double HaeufleTendonForceLengthCurve::calcValue(
+        double aSerialElasticLength) const {
+    double Lsee0 = getSerialElasticRestLength();
+    double Fsee =
+            0; // initialize Fsee for first case aSerialElasticLength < Lsee0
+    double deltaUseenll = getRelativeStretchAtNonlinearLinearTransition();
+    if (aSerialElasticLength > Lsee0 &&
+            aSerialElasticLength <= (Lsee0 * (1 + deltaUseenll))) 
+    {
+        double deltaFsee0 = getForceAtNonlinearLinearTransition();
+        double nuesee = deltaUseenll / getRelativeStretchAtLinearPart();
+        Fsee = deltaFsee0 *
+               pow((aSerialElasticLength - Lsee0) / (deltaUseenll * Lsee0),
+                       nuesee);
+    } 
+    else if (aSerialElasticLength > (Lsee0*(1+deltaUseenll))) 
+    {
+        double deltaFsee0 = getForceAtNonlinearLinearTransition();
+        double deltaUseel = getRelativeStretchAtLinearPart();
+        Fsee = deltaFsee0 *
+               (1 + (aSerialElasticLength - Lsee0 * (1 + deltaUseenll)) /
+                               (deltaUseel * Lsee0));
+    }
+    return Fsee;
+}
+
---
+++ OpenSim/Actuators/HaeufleTendonForceLengthCurve.h
@@ -0,0 +1,158 @@
+#ifndef OPENSIM_HAEUFLE_TENDON_FORCE_LENGTH_CURVE_H_
+#define OPENSIM_HAEUFLE_TENDON_FORCE_LENGTH_CURVE_H_
+/* -------------------------------------------------------------------------- *
+ *                     OpenSim:  HaeufleTendonForceLengthCurve.h                     *
+ * -------------------------------------------------------------------------- *
+ * The OpenSim API is a toolkit for musculoskeletal modeling and simulation.  *
+ * See http://opensim.stanford.edu and the NOTICE file for more information.  *
+ * OpenSim is developed at Stanford University and supported by the US        *
+ * National Institutes of Health (U54 GM072970, R24 HD065690) and by DARPA    *
+ * through the Warrior Web program.                                           *
+ *                                                                            *
+ * Copyright (c) 2005-2017 Stanford University and the Authors                *
+ * Author(s): Mike Spahr                                                      *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may    *
+ * not use this file except in compliance with the License. You may obtain a  *
+ * copy of the License at http://www.apache.org/licenses/LICENSE-2.0.         *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ * -------------------------------------------------------------------------- */
+
+// INCLUDE
+#include <OpenSim/Actuators/osimActuatorsDLL.h>
+#include <OpenSim/Simulation/Model/ModelComponent.h>
+
+#ifdef SWIG
+    #ifdef OSIMACTUATORS_API
+        #undef OSIMACTUATORS_API
+        #define OSIMACTUATORS_API
+    #endif
+#endif
+
+namespace OpenSim {
+/** This class serves as a TendonForceLengthCurve as described by Guenther et al
+    in 2007. 
+    In contrast to the quintic bezier splines, which is used in the 
+    TendonForceLengthCurve, this implementation uses a segmentwise defined
+    function with the following parameters.
+
+    @param serialElasticRestLength
+    @param relativeStretchAtNonlinearLinearTransition
+    @param relativeStretchAtLinearPart
+    @param forceAtNonlinearLinearTransition
+   
+
+    <B>Default Parameter Values</B>
+    \verbatim
+    serialElasticRestLength ...................... 0.15
+    relativeStretchAtNonlinearLinearTransition ... 0.0425
+    relativeStretchAtLinearPart .................. 0.0170
+    forceAtNonlinearLinearTransition ............. 500
+    \endverbatim
+
+    <B>Example</B>
+    @code
+        HaeufleTendonForceLengthCurve fseCurve(0.15, 0.0425, 0.0170, 500);
+        double fseVal   = fseCurve.calcValue(0.02);
+    @endcode
+
+    <B>References</B>
+    \li D.F.B. Haeufle, M. Guenther, A. Bayer, S. Schmitt(2014) Hill-type
+        muscle model with serial damping and eccentric force–velocity
+        relation. Journal of Biomechanics
+
+    @author Mike Spahr
+*/
+class OSIMACTUATORS_API HaeufleTendonForceLengthCurve : public ModelComponent {
+    OpenSim_DECLARE_CONCRETE_OBJECT(
+            HaeufleTendonForceLengthCurve, ModelComponent);
+
+public:
+    //==============================================================================
+    // PROPERTIES
+    //==============================================================================
+    OpenSim_DECLARE_PROPERTY(serial_elastic_rest_length, double,"");
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(
+            relative_stretch_at_nonlinear_linear_transition, double,"");
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(relative_stretch_at_linear_part, double,
+            "");
+    OpenSim_DECLARE_OPTIONAL_PROPERTY(force_at_nonlinear_linear_transition, double,
+            "");
+
+    //==============================================================================
+    // PUBLIC METHODS
+    //==============================================================================
+    /** The default constructor creates a tendon-force-length curve using the
+    default property values and assigns a default name. */
+    HaeufleTendonForceLengthCurve();
+
+    /** Constructs a tendon-force-length curve using the muscle dependen parameters and
+    assigns a default name. */
+    HaeufleTendonForceLengthCurve(double serialElasticRestLength,
+            double forceAtNonlinearLinearTransition);
+
+    /** Constructs a tendon-force-length curve using the provided parameters and
+    assigns a default name. */
+    HaeufleTendonForceLengthCurve(double serialElasticRestLength,
+            double relativeStretchAtNonlinearLinearTransition,
+            double relativeStretchAtLinearPart,
+            double forceAtNonlinearLinearTransition);
+
+    /** @returns The serial elastic rest length Lsee,0 */
+    double getSerialElasticRestLength() const;
+    
+    /** @returns The relative stretch at nonlinear/linear transition in Fsee */
+    double getRelativeStretchAtNonlinearLinearTransition() const;
+    
+    /** @returns The relative stretch in the linear part for force increase
+     * delta Fsee,0 */
+    double getRelativeStretchAtLinearPart() const;
+    
+    /** @returns The force at nonlinear/linear transition in Fsee */
+    double getForceAtNonlinearLinearTransition() const;
+
+    /** 
+    @param aSerialElasticRestLength
+        The resting length of the serial elastic element
+    */
+    void setSerialElasticRestLength(double aSerialElasticRestLength ) const;
+   
+    /**
+    @param aForceAtNonlinearLinearTransition
+        The force at non-linear/linear transition which is muscle specific
+    */
+    void setForceAtNonlinearLinearTransition(double aForceAtNonlinearLinearTransition ) const;
+    
+    /**
+    @param aForceAtNonlinearLinearTransition
+        The force at non-linear/linear transition which is muscle specific
+    */
+    void setRelativeStretchAtNonlinearLinearTransition(double aRelativeStretchAtNonlinearLinearTransition ) const;
+    
+    /**
+    @param aForceAtNonlinearLinearTransition
+        The force at non-linear/linear transition which is muscle specific
+    */
+    void setRelativeStretchAtLinearPart(double aRelativeStretchAtLinearPart ) const;
+
+
+    /** Evaluates the tendon-force-length curve at a tendon length of
+    'aSerialElasticLength'. */
+    double calcValue(double aSerialElasticLength) const;
+
+    //==============================================================================
+    // PRIVATE
+    //==============================================================================
+private:
+    void setNull();
+    void constructProperties();
+};
+
+} // namespace OpenSim
+
+#endif // OPENSIM_HAEUFLE_TENDON_FORCE_LENGTH_CURVE_H_
